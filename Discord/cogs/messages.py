0c71c6d734240aa63c4d460bd074180245ed1be7
Elliott Hager <ehager2@kent.edu>
2025-03-08 12:59:54 -0500

    Moved everything to threads, thread base before adding more features to it

--- Discord/cogs/messages.py
+++ Discord/cogs/messages.py
@@ -33,12 +33,21 @@ class Messages(commands.Cog):
     async def listGames(self, interaction: discord.Interaction):
 
         #Call API HERE and collect titles!
+        games = requests.get("http://127.0.0.1:5000/catalogue/titles")
+
+        #games = requests.post("http://127.0.0.1:5000/catalogue/search", json="{title: 'T'}")
+
+        games = games.json()
+
+
+        print(games)
 
         gameString = f''
 
         #Go through the games and add them as bullet points
         for game in games:
-            gameString += f'\n * {game}'
+            print(type(game))
+            gameString += f'\n * {game["title"]}'
 
 
         #Send the games back in an message
@@ -47,25 +56,26 @@ class Messages(commands.Cog):
     
     
     @app_commands.command(name="create_event", description="Creates an event and allows users to RSVP")
-    #@app_commands.describe(eventName="the name of the event")
-    async def createEvent(self, interaction: discord.Interaction):
-        #process the event with the API, and make response based on the code returned
+    @app_commands.describe(event_name="the name of the event")
+    async def createEvent(self, interaction: discord.Interaction, event_name: str):
+        usersID = interaction.user.id
 
-        #await interaction.response.send_message("Event has been scheduled successfully!")
+        await interaction.response.pong()
+        
 
         #Send a message to create a thread on (have to without the server being Nitro boosted)
-        threadStarter = (await interaction.response.send_message("Creating a thread to schedule, please open the thread to continue (The thread will be deleted afterwards for privacy)")).resource
+        threadStarter = (await interaction.response.send_message("Creating a thread to schedule, please open the thread to continue (The thread will be locked after)")).resource
 
         #
         #   Thread creation
         #
-        await threadStarter.create_thread(name="Test Thread")
+        thread = await threadStarter.create_thread(name="Test Thread")
 
         #Ask if the person would like to book the private room for the event?
-        await interaction.response.send_message("Would you like to book a private room for the event (Can do half and full room)? \n\n 👍 - Yes \n\n 👎- No ")
+        privateRoomWanted = await thread.send("Would you like to book a private room for the event (Can do half and full room)? \n\n 👍 - Yes \n\n 👎- No ")
 
         #Grab the message to then add the valid reactions to make it easier on the user
-        privateRoomWanted = await interaction.original_response()
+        # = await interaction.original_response()
 
         #Adds in the options for valid reactions
         await privateRoomWanted.add_reaction("👍")
@@ -75,13 +85,15 @@ class Messages(commands.Cog):
         try:
             reaction, user = await self.bot.wait_for('reaction_add', timeout=60)
             if str(reaction.emoji) == "👍":
-                await interaction.followup.send("If you would like to book the backroom for this event, please visit one of the two links below:\n HALF ROOM \n FULL ROOM")
+                await thread.send("If you would like to book the backroom for this event, please visit one of the two links below:\n HALF ROOM \n FULL ROOM")
             #elif str(reaction.emoji) == "👎":
                 #do nothing or still deciding on what to do
         except TimeoutError:
-            await interaction.followup.send("Time out awaiting for a reaction, please try again")
+            await thread.send("Time out awaiting for a reaction, please try again")
         
-        await interaction.followup.send("Thank you for scheduling your event!")
+        await interaction.followup.send("Thank you for scheduling your event, the thread is now locked!")
+
+        await thread.edit(archived=True, locked=True)
 
     #@app_commands.command()
     #async def 
